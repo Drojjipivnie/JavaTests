8024479bc69e5b3bfdf594a054e828190e2b8bf1
drojjipivnie <drojjipivnie@gmail.com>
2018-01-04 19:33:19 +0300

    Fixed something

--- app/src/main/java/com/drojj/javatests/base/MainApplication.java
+++ app/src/main/java/com/drojj/javatests/base/MainApplication.java
@@ -3,6 +3,8 @@ package com.drojj.javatests.base;
 import android.app.Application;
 
 import com.drojj.javatests.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
 import com.yandex.metrica.YandexMetrica;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -15,9 +17,11 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        FirebaseApp.initializeApp(this);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
         YandexMetrica.activate(getApplicationContext(), YANDEX_API_KEY);
         YandexMetrica.enableActivityAutoTracking(this);
-
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/NotoSans-Regular.ttf")
